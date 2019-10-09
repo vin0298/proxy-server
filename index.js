e370848f3c7dad89a819ddd4fd97d5f5de70e72b
Vincent Surya Bensawan <bensawan0298@gmail.com>
2019-10-08 23:59:12 -0700

    Used absolutify to change URLs and added two helper functions

--- index.js
+++ index.js
@@ -1,10 +1,82 @@
-var http = require('http');
-var url = require('url');
-var request = require('request');
-var serverPort = 5000;
+const http = require('http');
+const url = require('url');
+const request = require('request');
+const net = require('net');
+const express = require('express');
+const absolutify = require('absolutify');
+const app = express();
+const async = require('async');
 
-http.createServer(onRequest).listen(serverPort);
+const serverPort = 5000;
+const MAX_TIMESTAMP = 8640000000000000;
 
-function onRequest(req, res) {
-    console.log("I got it! " + req);
+const redis = require('redis'),
+    client = redis.createClient();
+client.on('connect', ()=> console.log('Redis connected to Proxy Server'));
+
+app.listen(serverPort);
+
+app.get('/', function(request, response) {
+    let targetURL = request.query;
+
+});
+
+function checkCacheAndSendResponse(req, res, targetURL) {
+    client.get(targetURL, (err, result) => {
+        var curDate = Date.now();
+        
+        // TODO: change expirationTime
+        if (err == null && result != null && !checkIfStale(curDate, JSON.parse(result).expirationTime)) {
+            console.log("sending from memory");
+            res.send(JSON.parse(result).response);
+        } else {
+            request(targetURL, function(error, response, body) {
+                if (!error && parseInt(response.statusCode) == 200) { 
+                    var expirationTimeToStore = MAX_TIMESTAMP;
+                    if (response.headers['cache-control']) {
+                        expirationTimeToStore = Date.now() + parseForMaxAge(response.headers['cache-control']);
+                    }
+
+                    body = absolutify(body, function(url, attrName) {
+                        return '/?url=' + targetURL + url;
+                    })
+
+                    const urlToStore = {
+                        response: response,
+                        body: body,
+                        expirationTime: expirationTimeToStore;
+                    }
+
+                    client.set(targetURL, JSON.stringify(urlToStore), (err, reply) => {
+                        if (reply == 'OK') {
+                            res.send(body);
+                        }
+                    })
+                } else {
+                    console.log("invalid url: " + error);
+                }
+            })
+        }
+    })
+}
+
+function parseForMaxAge(cacheControl) {
+     var parsedArr = cacheControl.split(',');
+     for(i = 0; i < parsedArr.length; i++) {
+         if (parsedArr[i].includes("max-age=")) {
+            return parseInt(parsedArr[i].substring(8)) * 1000;
+         }
+     }
+ }
+
+function checkIfStale(curTime, expirationTimeString) {
+    var expirationTime = new Date(parseInt(expirationTimeString));
+
+    if (expirationTime == MAX_TIMESTAMP) return false;
+
+    return (curTime > expirationTime)? true : false;
+}
+
+function displayCache() {
+
 }
